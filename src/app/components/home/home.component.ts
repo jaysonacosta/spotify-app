1c15b63a3833a7fd2ab2a65095f73e44b6a02ed0
Jayson Acosta <jaysonacosta540@gmail.com>
2021-06-05 20:31:31 -0400

    Showing users top 9 artists

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,18 +12,111 @@ export class HomeComponent implements OnInit {
     artistName: '',
   };
 
+  user: User = {
+    name: '',
+    email: '',
+    id: '',
+    country: '',
+  };
+
+  topArtists: TopArtists = {
+    artists: [],
+  };
+
+  private accessToken: any;
+
   constructor(
     private spotifyService: SpotifyService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
-  ngOnInit(): void {
-    this.activatedRoute.queryParams.subscribe((params: Params) => {
-      this.spotifyService.accessTokenExchange(params.code);
-    });
+  async ngOnInit(): Promise<void> {
+    if (localStorage.length === 0) {
+      this.activatedRoute.queryParams.subscribe((params: Params) => {
+        this.spotifyService.accessTokenExchange(params.code).subscribe(
+          (res: any) => {
+            console.log(res);
+            this.accessToken = res.access_token;
+            localStorage.setItem('access_token', this.accessToken);
+          },
+          (err) => {
+            console.log(err);
+            this.router.navigate(['']);
+          }
+        );
+      });
+    } else {
+      this.accessToken = localStorage.getItem('access_token');
+      this.spotifyService.getCurrentUser(this.accessToken).subscribe(
+        (res) => {
+          return;
+        },
+        (err) => {
+          localStorage.clear();
+          this.router.navigate(['']);
+        }
+      );
+    }
+    this.getCurrentUser(this.accessToken);
+    this.getTopArtists(this.accessToken);
+  }
+
+  getCurrentUser(accessToken: string): void {
+    this.spotifyService.getCurrentUser(accessToken).subscribe(
+      (res: any) => {
+        this.user.name = res.display_name;
+        this.user.email = res.email;
+        this.user.id = res.id;
+        this.user.country = res.country;
+      },
+      (err) => {
+        this.router.navigate(['']);
+      }
+    );
+  }
+
+  getTopArtists(accessToken: string): void {
+    this.spotifyService.getTopArtists(accessToken).subscribe(
+      (res: any) => {
+        for (let i = 0; i < 9; i++) {
+          const entry = res.items[i];
+          const artist: Artist = {
+            name: entry.name,
+            totalFollowers: entry.followers.total,
+            genres: entry.genres[0],
+            imageUrl: entry.images[0].url,
+            uri: entry.uri,
+          };
+          this.topArtists.artists.push(artist);
+        }
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 }
 
-export interface Search {
+interface Search {
   artistName: string;
+}
+
+interface User {
+  name: string;
+  email: string;
+  id: string;
+  country: string;
+}
+
+interface TopArtists {
+  artists: Artist[];
+}
+
+interface Artist {
+  name: string;
+  totalFollowers: number;
+  genres: string[];
+  imageUrl: string;
+  uri: string;
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,14 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class SpotifyService {
   private spotifyUrl: string = 'https://accounts.spotify.com/';
+  private apiUrl: string = 'https://api.spotify.com/';
   private clientId: string = '5cdb8b04c6fa4658b0c9a6329a2e88f1';
   private clientSecret: string = '129240bc03cb4c3984e637ecf61dd543';
   private redirectUri: string = 'http://localhost:4200/home';
-  private scopes: string = 'user-read-private user-read-email';
+  private scopes: string =
+    'user-read-private user-read-email user-read-recently-played user-top-read';
 
   constructor(private http: HttpClient) {}
 
-  authorizeUser() {
+  async authorizeUser(): Promise<void> {
     const url =
       this.spotifyUrl +
       'authorize?client_id=' +
@@ -21,30 +23,46 @@ export class SpotifyService {
       '&response_type=code&redirect_uri=' +
       this.redirectUri +
       '&scope=' +
-      this.scopes;
-    return window.open(url);
+      this.scopes +
+      '&state=34fFs29kd09';
+    window.open(url, '_self');
   }
 
   accessTokenExchange(authorizationCode: string) {
     const url = this.spotifyUrl + 'api/token';
-    const header = new HttpHeaders({
-      'Authorization': `Basic ${btoa(this.clientId)}:${btoa(this.clientSecret)}`,
+    const params = new HttpParams({
+      fromObject: {
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        redirect_uri: this.redirectUri,
+      },
     });
-    console.log(header);
-    this.http
-      .post(
-        url,
-        {
-          grant_type: 'authorization_code',
-          code: authorizationCode,
-          redirect_uri: this.redirectUri,
-        },
-        {
-          headers: header,
-        }
-      )
-      .subscribe((res) => {
-        console.log(res);
-      });
+    const base64 = btoa(this.clientId + ':' + this.clientSecret);
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Basic ' + base64,
+      }),
+    };
+    return this.http.post(url, params, httpOptions);
+  }
+
+  getCurrentUser(accessToken: string) {
+    const url = this.apiUrl + 'v1/me';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + accessToken,
+      }),
+    };
+    return this.http.get(url, httpOptions);
+  }
+
+  getTopArtists(accessToken: string) {
+    const url = this.apiUrl + 'v1/me/top/artists';
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer ' + accessToken,
+      }),
+    };
+    return this.http.get(url, httpOptions);
   }
 }
